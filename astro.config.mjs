62d38b10f00c19d9c147ce4f6841332cd6d59257
Your Name <you@example.com>
2025-02-12 01:33:19 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
+
 
 // https://astro.build/config
 export default defineConfig({
