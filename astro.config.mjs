bc5aaf0b100f36999fa49db4d66ee78d4a3bf24c
Your Name <you@example.com>
2025-02-12 01:20:04 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,10 @@ import node from "@astrojs/node";
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: node({
-    mode: "standalone"
-  }),
+//  adapter: node({
+//    mode: "standalone"
+//  }),
+  adapter: vercel(),
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 3000, // Usa el puerto de Render o el 3000 por defecto
